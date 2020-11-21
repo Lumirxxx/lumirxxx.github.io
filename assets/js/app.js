79588ce030a52cf1cc27b1bd81d39f807f60b4f2
Lumir <lumirxxx@gmail.com>
2020-11-21 17:11:26 +0300

    Add White Page and custom slider

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$('.owl-carousel').owlCarousel({
+    items:1,
+    loop:false,
+    dots:false,
+    center:true,
+    margin:10,
+    URLhashListener:true,
+    autoplayHoverPause:true,
+    startPosition: 'URLHash'
+});
