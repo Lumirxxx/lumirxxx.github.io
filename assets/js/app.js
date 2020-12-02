e4f8d440887e802bf705e2555564efa4bb223521
Lumir <lumirxxx@gmail.com>
2020-12-02 22:43:55 +0300

    Adaptiv litle page

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,19 @@ $('.owl-carousel').owlCarousel({
     autoplayHoverPause:true,
     startPosition: 'URLHash'
 });
+
+
+ $('.select-peoples .select-peoples__val').click(function(){
+        $('.select-peoples__dd').slideToggle(300);
+    });
+
+    $(document).mouseup(function (e) {
+        if (e.which != 1) return false;
+        var div = $(".select-peoples__dd");
+        var button = $('.select-peoples .select-peoples__val');
+        if (!div.is(e.target) && div.has(e.target).length === 0) {
+            if (!button.is(e.target) && button.has(e.target).length === 0) {
+             $('.select-peoples__dd').slideUp(300);
+         }
+     }
+ });
